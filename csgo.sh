ab97391a05afbd11a74ab268a9fb85e84bb12ed5
Patrick Richter <patrick@ruepplein.de>
2019-10-18 23:31:33 +0200

    Update csgo.sh

--- csgo.sh
+++ csgo.sh
@@ -1,1 +1,3 @@
 ./steamcmd.sh +login anonymous +force_install_dir /srv/csgo +app_update 740 validate +quit
+
+csgo -console -usercon +game_type $GAME_TYPE +game_mode $GAME_MODE +sv_setsteamaccount $STEAM_TOKEN -net_port_try 1 +mapgroup mg_armsrace +map ar_shoots
