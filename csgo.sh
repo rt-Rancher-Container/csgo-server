a73feb001a9f34ad2f6fd6af67298c3417292e3b
Patrick Richter <patrick@ruepplein.de>
2019-10-19 21:10:23 +0200

    Update csgo.sh

--- csgo.sh
+++ csgo.sh
@@ -3,4 +3,4 @@ then
     ./steamcmd.sh +login anonymous +force_install_dir /srv/csgo +app_update 740 validate +quit
 fi
 
-/srv/csgo/srcds_run -game csgo -console -autoupdate -usercon +fps_max 144 -tickrate 128 -port $PORT +hostname $HOSTNAME +game_type $GAME_TYPE +game_mode $GAME_MODE +sv_setsteamaccount $STEAM_TOKEN +authkey $AUTHKEY -net_port_try 1 +mapgroup mg_armsrace +map ar_shoots +rcon_password lkigenio +sv_password lkigenio
+/srv/csgo/srcds_run -game csgo -console -autoupdate -usercon +fps_max 144 -tickrate 128 -port $PORT +hostname $HOSTNAME +game_type $GAME_TYPE +game_mode $GAME_MODE +sv_setsteamaccount $STEAM_TOKEN -authkey $AUTHKEY -net_port_try 1 +mapgroup mg_armsrace +map ar_shoots +rcon_password lkigenio +sv_password lkigenio
