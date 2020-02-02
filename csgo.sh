389c3e39c98cd8eb0ecb26c5b0e1b1a1e6e9c9c7
Patrick Richter <patrick@ruepplein.de>
2020-02-02 05:30:17 +0100

    Update csgo.sh

--- csgo.sh
+++ csgo.sh
@@ -1,6 +1,9 @@
-if ! [ -e /srv/csgo/srcds_run ]
-then
-    ./steamcmd.sh +login anonymous +force_install_dir /srv/csgo +app_update 740 validate +quit
-fi
+#!/bin/bash
 
+# update server's data
+./steamcmd.sh +login anonymous +force_install_dir /srv/csgo +app_update 740 validate +exit
+
+# start the server
 /srv/csgo/srcds_run -game csgo -console -autoupdate -usercon +fps_max 144 -tickrate 128 -port $PORT +hostname $HOSTNAME +game_type $GAME_TYPE +game_mode $GAME_MODE +sv_setsteamaccount $STEAM_TOKEN -authkey $AUTHKEY -net_port_try 1 +mapgroup mg_armsrace +map ar_shoots +rcon_password lkigenio +sv_password lkigenio
+
+exit 0
